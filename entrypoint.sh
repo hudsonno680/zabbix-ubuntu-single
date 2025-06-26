60ab407f450d6b8eb306027df2e314570477e32c
Hudson Oliveira <hudsonno@msn.com>
2025-06-26 16:18:27 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+service mysql start
+
+mysql -uroot <<EOF
+CREATE DATABASE IF NOT EXISTS zabbix CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+CREATE USER IF NOT EXISTS 'zabbix'@'localhost' IDENTIFIED BY 'zabbix';
+GRANT ALL PRIVILEGES ON zabbix.* TO 'zabbix'@'localhost';
+FLUSH PRIVILEGES;
+EOF
+
+zcat /usr/share/doc/zabbix-sql-scripts/mysql/server.sql.gz | mysql -uzabbix -pzabbix zabbix
+
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
