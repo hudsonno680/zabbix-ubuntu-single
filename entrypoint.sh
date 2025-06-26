d338aab5a1dbc5e9a3d8d04bcc914280b8b4ba26
Hudson Oliveira <hudsonno@msn.com>
2025-06-26 18:25:47 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,21 +3,23 @@
 service mysql start
 
 mysql -uroot <<EOF
-create database zabbix character set utf8mb4 collate utf8mb4_bin;
-create user zabbix@localhost identified by 'zabbix';
-grant all privileges on zabbix.* to zabbix@localhost;
-set global log_bin_trust_function_creators = 1;
+CREATE DATABASE IF NOT EXISTS zabbix CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+CREATE USER IF NOT EXISTS 'zabbix'@'localhost' IDENTIFIED BY 'zabbix';
+GRANT ALL PRIVILEGES ON zabbix.* TO 'zabbix'@'localhost';
+SET GLOBAL log_bin_trust_function_creators = 1;
 EOF
 
 zcat /usr/share/doc/zabbix-sql-scripts/mysql/server.sql.gz | mysql -uzabbix -pzabbix zabbix
 
-mysql -uroot <<EOF
-mysql> set global log_bin_trust_function_creators = 0;
-EOF
+# Remove flag após importação de funções
+mysql -uroot -e "SET GLOBAL log_bin_trust_function_creators = 0;"
 
+# Insere senha do banco no config do servidor
 sed -i 's/^#\?DBPassword=.*/DBPassword=zabbix/' /etc/zabbix/zabbix_server.conf
 
+# Inicia serviços via systemd (se suportado) e habilita na inicialização
 systemctl restart zabbix-server zabbix-agent apache2
-systemctl enable zabbix-server zabbix-agent apache2 
+systemctl enable zabbix-server zabbix-agent apache2
 
+# Inicia supervisord 
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
