06b64b10d228ab2fbbc7a693c6ebf49cd8a0c651
Hudson Oliveira <hudsonno@msn.com>
2025-06-26 17:57:34 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,21 @@
 service mysql start
 
 mysql -uroot <<EOF
-CREATE DATABASE IF NOT EXISTS zabbix CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-CREATE USER IF NOT EXISTS 'zabbix'@'localhost' IDENTIFIED BY 'zabbix';
-GRANT ALL PRIVILEGES ON zabbix.* TO 'zabbix'@'localhost';
-FLUSH PRIVILEGES;
+create database zabbix character set utf8mb4 collate utf8mb4_bin;
+create user zabbix@localhost identified by 'zabbix';
+grant all privileges on zabbix.* to zabbix@localhost;
+set global log_bin_trust_function_creators = 1;
 EOF
 
 zcat /usr/share/doc/zabbix-sql-scripts/mysql/server.sql.gz | mysql -uzabbix -pzabbix zabbix
+
+mysql -uroot <<EOF
+mysql> set global log_bin_trust_function_creators = 0;
+EOF
+
+sed -i 's/^#\?DBPassword=.*/DBPassword=zabbix/' /etc/zabbix/zabbix_server.conf
+
+systemctl restart zabbix-server zabbix-agent apache2
+systemctl enable zabbix-server zabbix-agent apache2 
 
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
